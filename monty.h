16ac4c4dfce3fb0596d18111b2c5347407f85349
Emasco001 <malachy820@gmail.com>
2022-10-09 20:36:13 -0700

    monty header

--- monty.h
+++ monty.h
@@ -1,94 +1,47 @@
-#ifndef __MONTY_H__
-#define __MONTY_H__
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define STACK 1
-#define QUEUE 0
-#define DELIM " \n\t\r\a\b"
-#define FAILURE -1
-
-
+#include <unistd.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
+ *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
-
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
+
 
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
+ *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
-
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * struct global_s - global struct for monty variable
- * @err_status: error status to hold error term
- * @toks_num: number tokenized from the line read
- */
+void usage_error(void);
+void open_error(char *file_name);
+void malloc_error(char *holder);
+void unknown_error(FILE *f, stack_t *stack, char *content, char *line, int x);
+void free_dlistint(stack_t *head);
+stack_t *add_dnodeint(stack_t **head, const int n);
+size_t print_dlistint(const stack_t *h);
 
-typedef struct global_s
-{
-	int err_status;
-	char *toks_num;
-} global_t;
-
-extern global_t global;
-
-int exec_monty(FILE *);
-void (*get_op_func(char *ops))(stack_t **, unsigned int);
-stack_t *stack_init(void);
-int check_mode(stack_t *);
-
-
-int is_digit(char *);
-int stack_size(stack_t *);
-stack_t *add_new_node(int);
-
-
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void div(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
-void mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-void stack(stack_t **stack, unsigned int line_number);
-void queue(stack_t **stack, unsigned int line_number);
-
-
-
-void free_stack(stack_t **stack);
-
-
-
-
-#endif /* __MONTY_H__ */
+#endif
